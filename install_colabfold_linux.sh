2c96e3ad43630b1e45f1f06b592618f441141993
Tru Huynh <tru@pasteur.fr>
2021-09-28 17:20:50 +0200

    using my forked install_colabfold_linux.sh, upgrade jax version

--- install_colabfold_linux.sh
+++ install_colabfold_linux.sh
@@ -73,7 +73,7 @@ conda update -y conda
 echo "Installing conda-forge packages"
 conda install -c conda-forge python=3.7 cudnn==8.2.1.32 cudatoolkit==11.1.1 openmm==7.5.1 pdbfixer -y
 echo "Installing alphafold dependencies by pip"
-python3.7 -m pip install absl-py==0.13.0 biopython==1.79 chex==0.0.7 dm-haiku==0.0.4 dm-tree==0.1.6 immutabledict==2.0.0 jax==0.2.14 ml-collections==0.1.0 numpy==1.19.5 scipy==1.7.0 tensorflow-gpu==2.5.0
+python3.7 -m pip install absl-py==0.13.0 biopython==1.79 chex==0.0.7 dm-haiku==0.0.4 dm-tree==0.1.6 immutabledict==2.0.0 jax==0.2.21 ml-collections==0.1.0 numpy==1.19.5 scipy==1.7.0 tensorflow-gpu==2.5.0
 python3.7 -m pip install jupyter matplotlib py3Dmol tqdm
 python3.7 -m pip install --upgrade jax jaxlib==0.1.69+cuda111 -f https://storage.googleapis.com/jax-releases/jax_releases.html
 
