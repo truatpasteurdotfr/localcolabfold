1792dfe94f2e1e6050610c4166ae85e9a5d0fc9f
Tru Huynh <tru@pasteur.fr>
2022-11-15 15:56:43 +0100

    run-colabfold_batch.sh path changed

--- run-colabfold_batch.sh
+++ run-colabfold_batch.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-# 2021/11/15 adapted from /app/colabfold/bin/colabfold
-# force bash syntax for conda
-export CONDA_ROOT=/app/colabfold/conda
+# 2022/11/15 adapted from https://raw.githubusercontent.com/YoshitakaMo/localcolabfold/main/install_colabbatch_linux.sh
+# colabfold_batch
+export CONDA_ROOT=/app/colabfold_batch/conda
 eval "$(${CONDA_ROOT}/bin/conda shell.bash hook)"
-export COLABFOLDDIR=/app/colabfold
+export COLABFOLDDIR=/app/colabfold_batch
 conda activate ${COLABFOLDDIR}/colabfold-conda
 export PYTHONPATH=${COLABFOLDDIR}
 
-if [ ! -L alphafold ]; then
-echo "making alphafold -> /app/colabfold/alphafold as soft link"
-ln -s /app/colabfold/alphafold
-fi
+export TF_FORCE_UNIFIED_MEMORY="1"
+export XLA_PYTHON_CLIENT_MEM_FRACTION="4.0"
+export COLABFOLDDIR=/app/colabfold_batch
+export PATH="${COLABFOLDDIR}/colabfold-conda/bin:$PATH"
 
 if [ $# -ge 1 ]; then
 echo running python3.7 ${COLABFOLDDIR}/colabfold_batch "$@" from conda  ${COLABFOLDDIR}/colabfold-conda env
 python3.7 ${COLABFOLDDIR}/colabfold_batch "$@"
 fi
+
+
