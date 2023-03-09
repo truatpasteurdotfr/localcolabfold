0b77d6c94c71ae169a5b1a4397b2505a99ef3576
Tru Huynh <tru@pasteur.fr>
2023-03-09 19:56:44 +0100

    run-localcolabfold.sh replacing run-colabfold_batch.sh

---
+++ run-localcolabfold.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# run-localcolabfold.sh
+export CONDA_ROOT=/app/localcolabfold/conda
+eval "$(${CONDA_ROOT}/bin/conda shell.bash hook)"
+export COLABFOLDDIR=/app/localcolabfold
+conda activate ${COLABFOLDDIR}/colabfold-conda
+export PYTHONPATH=${COLABFOLDDIR}
+
+export TF_FORCE_UNIFIED_MEMORY="1"
+export XLA_PYTHON_CLIENT_MEM_FRACTION="4.0"
+export PATH="${COLABFOLDDIR}/colabfold-conda/bin:$PATH"
+
+echo running ${COLABFOLDDIR}/colabfold_batch "$@" from conda  ${COLABFOLDDIR}/colabfold-conda env
+${COLABFOLDDIR}/bin/colabfold_batch "$@"
+
+
